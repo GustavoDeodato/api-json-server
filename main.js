de6e4dcf57be52d4dad4062a5d493105f1c86395
GUSTAVO SILVA DEODATO <24122736@dsn127m1242375.cfp127aluno.local>
2025-07-31 16:01:17 -0300

    mock ok

---
+++ main.js
@@ -0,0 +1,33 @@
+{
+    "Fotos:"[
+    {
+    "id": 1,
+    "imagem": "https://drive.google.com/file/d/1pc1mTKuIXAWU3BvF0OTKSYwsjcl7ZTx-/view?usp=sharing",
+    "legenda": "onde o rocha comeu a minhoca",
+    "data": "01/01/25"
+},
+{
+    "id": 2,
+    "imagem": "https://drive.google.com/file/d/1pneLMW0BRAXri45losX_SvkbIO3Vzftx/view?usp=sharing",
+    "legenda": "voltinha no eletro",
+    "data": "01/01/25"
+},
+{
+    "id": 3,
+    "imagem": "https://drive.google.com/file/d/1pouuoqwL9sN-DPXZUUTAGy6rcBoriqAG/view?usp=sharing",
+    "legenda": "banheiro do almoço",
+    "data": "01/01/24"
+},
+{
+    "id": 4,
+    "imagem": "https://drive.google.com/file/d/1psJkv1rbVF9ZftezrzcgrQwYNVjnBWjW/view?usp=sharing",
+    "legenda": "bueno hibernava",
+    "data": "01/01/24"
+},
+{
+    "id": 5,
+    "imagem": "https://drive.google.com/file/d/1psYWjnzZqRhwO8vhU0FBOVD5XR6Wy2Q0/view?usp=sharing",
+    "legenda": "altos push na ranked",
+    "data": "01/01/24"
+}
+]}
